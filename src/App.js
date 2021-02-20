37c376846ae3ab781dc11b9d0200780c066b0836
brandontiu <brandon.tiu@mail.utoronto.ca>
2021-02-19 23:28:39 -0500

    saving work

--- src/App.js
+++ src/App.js
@@ -1,59 +1,123 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { DragDropContext } from 'react-beautiful-dnd';
+
+import TRASH from 'data.js';
+import { shuffle, getTimeLeft, move, GAME_STATE } from 'utils.js';
+
+import Modal from './components/Modal';
+import Header from './components/Header';
+import Dropzone from './components/Dropzone';
+
+const GAME_DURATION = 1000 * 30; // 30 seconds
+
+const initialState = {
+  // we initialize the state by populating the bench with a shuffled collection of heroes
+  bench: shuffle(TRASH),
+  [COMICS.DC]: [],
+  [COMICS.MARVEL]: [],
+  gameState: GAME_STATE.READY,
+  timeLeft: 0,
+};
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      greeting: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  state = initialState;
+  
+  startGame = () => {
+    this.currentDeadline = Date.now() + GAME_DURATION;
 
-  handleChange(event) {
-    this.setState({ name: event.target.value });
-  }
+    this.setState(
+      {
+        gameState: GAME_STATE.PLAYING,
+        timeLeft: getTimeLeft(this.currentDeadline),
+      },
+      this.gameLoop
+    );
+  };
 
-  handleSubmit(event) {
-    event.preventDefault();
-    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
-      .then(response => response.json())
-      .then(state => this.setState(state));
-  }
+  gameLoop = () => {
+    this.timer = setInterval(() => {
+      const timeLeft = getTimeLeft(this.currentDeadline);
+      const isTimeout = timeLeft <= 0;
+      if (isTimeout && this.timer) {
+        clearInterval(this.timer);
+      }
+
+      this.setState({
+        timeLeft: isTimeout ? 0 : timeLeft,
+        ...(isTimeout ? { gameState: GAME_STATE.DONE } : {}),
+      });
+    }, 1000);
+  };
+
+  endGame = () => {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+
+    this.setState({
+      gameState: GAME_STATE.DONE,
+    });
+  };
+
+  resetGame = () => {
+    this.setState(initialState);
+  };
+
+  onDragEnd = ({ source, destination }) => {
+    if (!destination) {
+      return;
+    }
+
+    this.setState(state => {
+      return move(state, source, destination);
+    });
+  };
 
   render() {
+    const { gameState, timeLeft, bench, ...groups } = this.state;
+    const isDropDisabled = gameState === GAME_STATE.DONE;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <form onSubmit={this.handleSubmit}>
-            <label htmlFor="name">Enter your name: </label>
-            <input
-              id="name"
-              type="text"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-            <button type="submit">Submit</button>
-          </form>
-          <p>{this.state.greeting}</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+          <>
+            <Header gameState={gameState} timeLeft={timeLeft} endGame={this.endGame} />
+            {this.state.gameState !== GAME_STATE.PLAYING && (
+              <Modal
+                startGame={this.startGame}
+                resetGame={this.resetGame}
+                timeLeft={timeLeft}
+                gameState={gameState}
+                groups={groups}
+              />
+            )}
+            {(this.state.gameState === GAME_STATE.PLAYING ||
+              this.state.gameState === GAME_STATE.DONE) && (
+              <DragDropContext onDragEnd={this.onDragEnd}>
+                <div className="container">
+                  <div className="columns">
+                    <Dropzone
+                      id={COMICS.MARVEL}
+                      heroes={this.state[COMICS.MARVEL]}
+                      isDropDisabled={isDropDisabled}
+                    />
+                    <Dropzone id="bench" heroes={bench} isDropDisabled={isDropDisabled} />
+                    <Dropzone
+                      id={COMICS.DC}
+                      heroes={this.state[COMICS.DC]}
+                      isDropDisabled={isDropDisabled}
+                    />
+                  </div>
+                </div>
+              </DragDropContext>
+            )}
+            <Footer />
+          </>
+        );
+      }
+
+  componentWillUnmount() {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
   }
 }
 
---
+++ src/data.js
@@ -0,0 +1,78 @@
+export const TRASH = [
+    {
+        name: 'used diapers',
+        type: 'garbage'
+    },
+    {
+        name: 'coffee cup',
+        type: 'garbage',
+    },
+    {
+        name: 'hair',
+        type: 'garbage'
+    },
+    {
+        name: 'cigarette butt',
+        type: 'garbage'
+    },
+    {
+        name: 'wrapper',
+        type: 'garbage'
+    },
+    {
+        name: 'chip bag',
+        type: 'garbage'
+    },
+    {
+        name: 'tissue',
+        type: 'garbage'
+    },
+    {
+        name: 'plastic bag',
+        type: 'recyclable'
+    },
+    {
+        name: 'coffee lid',
+        type: 'recyclable'
+    },
+    {
+        name: 'newspaper',
+        type: 'recyclable'
+    },
+    {
+        name: 'can',
+        type: 'recyclable'
+    },
+    {
+        name: 'labelled can',
+        type: 'recyclable'
+    },
+    {
+        name: 'paper towel',
+        type: 'recyclable'
+    },
+    {
+        name: 'banana peel',
+        type: 'compost'
+    },
+    {
+        name: 'coffee sleeve',
+        type: 'compost'
+    },
+    {
+        name: 'apple core',
+        type: 'compost'
+    },
+    {
+        name: 'egg shell',
+        type: 'compost'
+    },
+    {
+        name: 'vegetable scraps',
+        type: 'compost'
+    },
+    {
+        name: 'bone',
+        type: 'compost'
+    },
+];
