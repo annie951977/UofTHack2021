1d3647a85102f8455f4a980e221e21bff2b07cca
Usama Ali <usamaa18@gmail.com>
2021-02-20 10:35:54 -0700

    added socket.io basics

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 
 const app = express();
+
+app.use(express.static(__dirname + '/node_modules')); 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
@@ -12,6 +14,18 @@ app.get('/api/greeting', (req, res) => {
   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
 
-app.listen(3001, () =>
+const server = app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
 );
+
+const io = require('socket.io')(server);
+
+
+const game = require('./game.js')
+io.sockets.on('connection', function(socket) {
+  console.log('client connected');
+  socket.on('disconnect', function() {
+    console.log("client disconnected");
+  });
+  // game.initGame(io, socket);
+});
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { io } from "socket.io-client"
 import './index.css';
 import 'spectre.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+const socket = io();
+
 
